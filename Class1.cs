7bc5b6048bdc258fe0314d545b690902920db05d
vikas ghdoke <vikasghodke468@gmail.com>
2024-01-24 17:39:00 +0530

    fifth commit

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    internal class Class1<T> where T: IComparable<T>
+    {
+        public T FindMin(T[] Array)
+        {
+           if(Array.Length==0)
+            {
+                Console.WriteLine("Out of bound");
+            }
+            T min = Array[0];
+
+            for(int i = 1; i < Array.Length; i++)
+            {
+                if (Array[i].CompareTo(min) < 0)
+                {
+                    min= Array[i];
+                }
+            }
+            return min;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = { 54, 45, 67, 89, 65 };
+            float[] floats = {3.4f, 45.6f, 67.43f, 90.4f};
+            string[] strings = { "t", "g", "k", "m" };
+
+            Class1<int> intmin = new Class1<int>();
+            Class1<float> floatmin = new Class1<float>();
+            Class1<string> stringmin = new Class1<string>();
+
+            int minInt = intmin.FindMin(number);
+            float minfloat = floatmin.FindMin(floats);
+            string minstring = stringmin.FindMin(strings);
+
+            Console.WriteLine("Minimum Integer" + minInt);
+            Console.WriteLine("Minimum Float" + minfloat);
+            Console.WriteLine("Minimum string" + minstring);
+
+        }
+    }
+}
