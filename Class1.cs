263411404cb98d273d4a0cc8d6f85ca839f2e667
vikas ghdoke <vikasghodke468@gmail.com>
2024-01-24 15:01:48 +0530

    Secont Commit

---
+++ Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Generic_Method_2
+{
+    internal class Class1
+    {
+        public void GeneRicDelete<T>(T[] Array, T element)
+        {
+            List<T> list = new List<T>();
+
+            list = Array.ToList();
+
+
+            foreach (T t in list)
+            {
+                Console.Write(t+" ");
+            }
+            Console.WriteLine("");
+
+            list.Remove(element);
+
+            foreach (T t in list)
+            {
+                Console.Write(t+" ");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Generic_Method_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7 };
+            double[] doubles = { 12.3, 34.5, 67.8, 90.9 };
+            char[] chars = { 'a', 'b', 'c', 'd' };
+            Class1 c1 = new Class1();
+
+            c1.GeneRicDelete(numbers, 4);
+            c1.GeneRicDelete(chars,'c');
+            c1.GeneRicDelete(doubles, 34.5);
+        }
+    }
+}
