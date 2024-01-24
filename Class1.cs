fa60af0bf65ba84371eab20b8263fd9688099d69
vikas ghdoke <vikasghodke468@gmail.com>
2024-01-24 16:35:24 +0530

    third commit

---
+++ Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Generic_Method_3
+{
+    public class Class1<T>
+    {
+        public void Adelete(T[] Array,T element)
+        {
+            List<T> list = new List<T>();
+            list=Array.ToList();
+
+            foreach(T t in list)
+            {
+                Console.WriteLine(t);
+            }
+            Console.WriteLine(" ");
+
+            list.Remove(element); 
+
+            foreach(T t in list)
+            {
+                Console.WriteLine(t+" ");
+            }
+
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generic_Method_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7 };
+            double[] doubles = { 12.3, 34.5, 56.7, 32.4 };
+            char[] chars = { 'a', 'b', 'c', 'd' };
+
+            Class1<int> c1 = new Class1<int>();
+            Class1<double> c2 = new Class1<double>();
+            Class1<char> c3 = new Class1<char>();
+
+            c1.Adelete(numbers, 3);
+            c2.Adelete(doubles, 12.3);
+            c3.Adelete(chars, 'b');
+
+        }
+    }
+}
