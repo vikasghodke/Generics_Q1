54f0764afee2e163f6a40cee5738c1ed1a81bbfd
vikas ghdoke <vikasghodke468@gmail.com>
2024-01-24 12:40:13 +0530

    first Commit

---
+++ Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Generics_Method_1
+{
+    public class Class1
+    {
+        public int[] DeleteMetod(int[] numbers)
+        {
+            Console.WriteLine("Enter a Element to delete");
+            int value = Convert.ToInt32(Console.ReadLine());
+            var newarr = Array.Empty<int>();
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i]==value)
+                {
+                    continue;
+                }
+                else
+                {
+                    newarr = newarr.Append(numbers[i]).ToArray();
+                    Console.WriteLine(numbers[i]);  
+                }
+            }
+            return newarr;
+
+        }
+
+        public double[] DeleteMethod1(double[] doubles)
+        {
+            Console.WriteLine("Enter Elements to Delete");
+            double value = Convert.ToDouble(Console.ReadLine());
+            var newarr = Array.Empty<double>();
+            for(int i=0;i<doubles.Length;i++)
+            {
+                if (doubles[i] == value)
+                {
+                    continue;
+                }
+                else
+                {
+                    newarr = newarr.Append(doubles[i]).ToArray();
+                    Console.WriteLine(doubles[i]);
+                }
+            }
+            return newarr;
+        }
+        public char[] DeleteMethod2(char[] chars) 
+        {
+            Console.WriteLine("Enter a Character to Delete");
+            char value = Convert.ToChar(Console.ReadLine());
+            var newarr = Array.Empty<char>();
+            for(int i=0;i<chars.Length;i++)
+            {
+                if (chars[i] == value)
+                {
+                    continue;
+                }
+                else
+                {
+                    newarr = newarr.Append(chars[i]).ToArray();
+                    Console.WriteLine(chars[i]);
+                }
+            }
+            return newarr;
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Generics_Method_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7 };
+            double[] doubles = { 12.3, 345.5, 56.4, 89.9 };
+            char[] chars = { 'a', 'b', 'c', 'd' };
+
+            Class1 c1=new Class1();
+            c1.DeleteMetod(numbers);
+            c1.DeleteMethod1(doubles);
+            c1.DeleteMethod2(chars);
+
+        }
+    }
+}
